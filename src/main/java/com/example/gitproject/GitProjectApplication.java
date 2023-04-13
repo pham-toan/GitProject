538806e98f16404a1685169b97d9cbccabc78ad4
k_toan <k_toan@brycen.com.vn>
2023-04-13 13:59:47 +0700

    develop_feature commit_1

--- src/main/java/com/example/gitproject/GitProjectApplication.java
+++ src/main/java/com/example/gitproject/GitProjectApplication.java
@@ -8,7 +8,7 @@ public class GitProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitProjectApplication.class, args);
-		int number = 11;
+		int number = 12;
 	}
 
 }
